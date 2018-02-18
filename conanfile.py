eb9e956b191da29beb8b49520331fa08e167eef1
Croydon <cr0ydon@outlook.com>
2018-02-18 17:00:22 +0100

    Migrate the Protobuf package over to Bincrafters

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class gRPCConan(ConanFile):
     description = "Google's RPC library and framework."
     url = "https://github.com/inexorgame/conan-grpc.git"
     license = "Apache-2.0"
-    requires = "zlib/1.2.11@conan/stable", "OpenSSL/1.1.0g@conan/stable", "Protobuf/3.5.1@inexorgame/stable", "gflags/2.2.1@bincrafters/stable", "c-ares/1.14.0@inexorgame/testing"
+    requires = "zlib/1.2.11@conan/stable", "OpenSSL/1.1.0g@conan/stable", "protobuf/3.5.1@bincrafters/stable", "gflags/2.2.1@bincrafters/stable", "c-ares/1.14.0@inexorgame/testing"
     settings = "os", "compiler", "build_type", "arch"
     options = {
             "shared": [True, False],
