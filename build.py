18fa138c94d2f9b12c5b94adb722b70409a317b6
Croydon <cr0ydon@outlook.com>
2018-07-26 03:08:23 +0200

    Apply latest Bincrafters templates

--- build.py
+++ build.py
@@ -1,6 +1,11 @@
-from conan.packager import ConanMultiPackager
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from bincrafters import build_template_default
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="inexorgame", channel="stable")
-    builder.add_common_builds()
+
+    builder = build_template_default.get_builder()
+
     builder.run()
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,12 @@
-from conans import ConanFile, CMake, tools, RunEnvironment
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+from conans import ConanFile, CMake
 import os
 
 
-class grpcTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
@@ -10,18 +14,10 @@ class grpcTestConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-#        if self.settings.os == "Macos":
- #           self.run("cd bin; for LINK_DESTINATION in $(otool -L client | grep libproto | cut -f 1 -d' '); do install_name_tool -change \"$LINK_DESTINATION\" \"@executable_path/$(basename $LINK_DESTINATION)\" client; done")
 
     def imports(self):
         self.copy("*", "bin", "bin")
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
-                self.run(bin_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get("DYLD_LIBRARY_PATH", ""), bin_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get("LD_LIBRARY_PATH", ""), bin_path))
+        bin_path = os.path.join("bin", "test_package")
+        self.run(bin_path, run_environment=True)
