841b7ed0290e6c7afe540d4b928274bb88c87ddf
Croydon <cr0ydon@outlook.com>
2019-08-25 02:59:49 +0200

    Update CI scripts

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-set -x
+set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
+    unset PYENV_ROOT
+    curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+    export PATH="$HOME/.pyenv/bin:$PATH"
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 3.7.3
-    pyenv virtualenv 3.7.3 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
+
+    pip install cmake --upgrade
 fi
 
 pip install conan --upgrade
-pip install conan_package_tools
+pip install conan_package_tools bincrafters_package_tools
 
 conan user
