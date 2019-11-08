4c967148a254300414388141829079c503f20d77
Croydon <git@cr0ydon.com>
2019-11-08 08:54:51 +0100

    Revert back to using Bincrafters Protobuf
    as long as the CCI variants for Protobuf and Protoc aren't stable

--- conanfile.py
+++ conanfile.py
@@ -36,7 +36,7 @@ class grpcConan(ConanFile):
     requires = (
         "zlib/1.2.11",
         "openssl/1.0.2t",
-        "protobuf/3.9.1",
+        "protobuf/3.9.1@bincrafters/stable",
         "protoc_installer/3.9.1@bincrafters/stable",
         "c-ares/1.15.0@conan/stable"
     )
