57a19e75f790553b479ff0d66885078555ea1056
Carlos Gomes Martinho <kmartinho8@gmail.com>
2019-11-18 12:48:10 +0100

    fix: depend on OpenSSL instead of openssl

--- conanfile.py
+++ conanfile.py
@@ -33,7 +33,7 @@ class grpcConan(ConanFile):
 
     requires = (
         "zlib/1.2.11",
-        "openssl/1.0.2t",
+        "OpenSSL/1.0.2t",
         "protobuf/3.9.1@bincrafters/stable",
         "protoc_installer/3.9.1@bincrafters/stable",
         "c-ares/1.15.0@conan/stable"
