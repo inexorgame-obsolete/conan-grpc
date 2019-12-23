92c44a79c6d4bd1d51473108a6d8ca1d44b2076c
Croydon <git@cr0ydon.com>
2019-12-23 20:22:41 +0100

    Cleanup test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from conans import ConanFile, CMake, tools
 import os
-import threading
-import time
 
 
 class TestPackageConan(ConanFile):
@@ -15,9 +10,6 @@ class TestPackageConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-    def imports(self):
-        self.copy("*", "bin", "bin")
 
     def test(self):
         if not tools.cross_building(self.settings):
