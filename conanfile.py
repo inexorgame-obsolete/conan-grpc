ed91b331834eaf534c0cc781449c9593ee3ccd30
Croydon <git@cr0ydon.com>
2020-03-18 22:04:36 +0100

    Update to 1.27.3

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
+from conans.tools import Version
 import os
 
 
 class grpcConan(ConanFile):
     name = "grpc"
-    version = "1.26.0"
+    version = "1.27.3"
     description = "Google's RPC library and framework."
     topics = ("conan", "grpc", "rpc")
     url = "https://github.com/inexorgame/conan-grpc"
@@ -33,10 +34,11 @@ class grpcConan(ConanFile):
 
     requires = (
         "zlib/1.2.11",
-        "openssl/1.1.1d",
+        "openssl/1.1.1e",
         "protobuf/3.9.1@bincrafters/stable",
         "protoc_installer/3.9.1@bincrafters/stable",
-        "c-ares/1.15.0"
+        "c-ares/1.15.0",
+        "abseil/20200205"
     )
 
     def configure(self):
@@ -56,9 +58,10 @@ class grpcConan(ConanFile):
         # See #5
         tools.replace_in_file(cmake_path, "_gRPC_PROTOBUF_LIBRARIES", "CONAN_LIBS_PROTOBUF")
 
-        # See https://github.com/grpc/grpc/issues/21293 - OpenSSL 1.1.1+ doesn't work without
-        tools.replace_in_file(
-            cmake_path, "set(_gRPC_BASELIB_LIBRARIES wsock32 ws2_32)", "set(_gRPC_BASELIB_LIBRARIES wsock32 ws2_32 crypt32)")
+        if Version(self.version) < "1.27.0":
+            # See https://github.com/grpc/grpc/issues/21293 - OpenSSL 1.1.1+ doesn't work without
+            tools.replace_in_file(
+                cmake_path, "set(_gRPC_BASELIB_LIBRARIES wsock32 ws2_32)", "set(_gRPC_BASELIB_LIBRARIES wsock32 ws2_32 crypt32)")
 
         # cmake_find_package_multi is producing a c-ares::c-ares target, grpc is looking for c-ares::cares
         tools.replace_in_file(
