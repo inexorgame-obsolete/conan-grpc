136c9d2f15738d1061e7e6c5ae73db34e61c9ce7
Croydon <git@cr0ydon.com>
2020-10-02 16:32:59 +0200

    Delete build.py

--- build.py
+++
@@ -1,6 +0,0 @@
-from conan.packager import ConanMultiPackager
-
-if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name=None, pure_c=False)
-    builder.run()
