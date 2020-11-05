b572b5a50a37718f1ee89817a1bddc688e4d6ab1
Croydon <git@cr0ydon.com>
2020-11-05 16:03:45 +0100

    Use CCI's protobuf

--- conanfile.py
+++ conanfile.py
@@ -34,9 +34,7 @@ class grpcConan(ConanFile):
     requires = (
         "zlib/1.2.11",
         "openssl/1.1.1h",
-        # "protobuf/3.11.4",
-        "protobuf/3.9.1@bincrafters/stable",
-        "protoc_installer/3.9.1@bincrafters/stable",
+        "protobuf/3.9.1",
         "c-ares/1.15.0",
         "abseil/20200225.3"
     )
